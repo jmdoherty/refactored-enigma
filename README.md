a1c310a4c234819c99f03fc5d291238fa2c0b069
James Doherty <james.doherty@arctiq.ca>
2021-11-10 16:20:20 -0500

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # refactored-enigma
-Another app for throwing load at
+A fastapi app for throwing load at, using a redis back end
+
+Uses environment variables of REDIS_HOST and REDIS_PORT for Redis backend
+Assumes localhost:6379 if not set
+
+## Routes: <br>
+GET /<name> - Return value of key <name> <br>
+POST /<name> - Increment value of key <name> by 1 and return that value <br>
+
+### Optional Query Parameters to increase resource usage to service a request <br>
+  * method - One of error, loop, memory, sleep <br>
+  * number - An integer used by method <br>
+
+### Methods: <br>
+* error - 1 in _number_ chancee it generates a 418 error <br>
+* loop - Loops 1M x _number_ times before completing request <br>
+* memory - Create a _number_ MB string in memory before it does so, in increments of 1MB before completing request <br>
+* slow - Randomally sleeps for up to _number_ ms before completing the request <br>
+
+## Sample load generation <br>
+
+### Using Curl <br>
+Get the value of somevalue <br>
+curl -X GET 'http://localhost:8000/somevalue' <br>
+Increase the value of somevalue by 1, use the sleep method to sleep up 1000ms <br>
+curl -X POST 'http://localhost:8000/somevalue?method=sleep&number=1000' <br>
+
+### Using wget <br>
+Get the value of blahh, looping 5M times before returning <br>
+wget 'http://localhost:8000/blahh?method=loop&number=5' <br>
+Get the value of blahh, sleeping up to 5000mss before returning <br>
+wget -qO- --post-data '' http://localhost:8000/blahh?method=sleep&number=5000 <br>
+
+### Using Apache Bench <br>
+Make a 100 requests to get the value of athing at a concurrency of 20
+Using the error method to get an error 1:5 times
+ab -n 100 -c 20  http://127.0.0.1:8000/athing\?method\=error\&number\=5
+Make a 100 requests to increment the value of athing at a concurrency of 20
+Using the sleep method to have each request sleep for up to 100ms
+ab -n 100 -c 20 -l -p /dev/null -T application/x-www-form-urlencoded http://127.0.0.1:8000/athing/sleep/100
+
+### Using siege <br>
+Siege the server, incrementing the value of what but getting an error 1 in 10 times
+siege 'http://localhost:8000/what?method=error&number=10 POST'
+
---
+++ main.py
@@ -0,0 +1,88 @@
+import os
+import redis
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import PlainTextResponse
+from random import randint
+from time import sleep
+
+backend = redis.Redis(host=os.environ.get('REDIS_HOST', 'localhost'),
+                      port=int(os.environ.get('REDIS_PORT', '6379')))
+
+MEGA=1024*1024
+response="Hello World"
+
+app = FastAPI()
+
+# Gets the value of 'name'
+@app.get('/{name}/{method}/{number}', response_class=PlainTextResponse)
+@app.get('/{name}', response_class=PlainTextResponse)
+def read(name: str, method: str = 'normal', number: int = 10):
+    
+    # Get current value of name, 0 if it doesnt exist  
+    value = backend.get(name)
+    if value is None:
+        value = 0
+   
+    # Use up some resources
+    responseLoad(method,number) 
+    
+    # Return current value of name 
+    return str(int(value)) + "\n"
+
+
+# Increments the value of 'name' by 1
+@app.post('/{name}/{method}/{number}', response_class=PlainTextResponse)
+@app.post('/{name}', response_class=PlainTextResponse)
+def read_item(name: str, method: str = 'normal', number: int = 10):
+    $ Increment value of name in redis
+    value = backend.incr(name)
+    
+    # Use up some resources 
+    responseLoad(method,number) 
+   
+    # Return new value of name 
+    return str(value) + "\n"
+
+# Make some load
+def responseLoad(method,number):
+  if method == 'normal':
+    pass 
+  elif method == 'error':
+    error_load(number)
+  elif method == 'loop':
+    loop_load(number)
+  elif method == 'sleep':
+    sleep_load(number)
+  elif method == 'memory':
+    memory_load(number)
+
+# Generate an error at chance of 1/number
+def error_load(number: int):
+  if randint(1,int(number)) == 1:
+    raise HTTPException(status_code=418, detail="A mysterious error")
+
+# Loop number million times
+def loop_load(number: int):
+  i = 0
+  while i < number:
+     j = 0 
+     while j < MEGA:
+       j += 1  
+     i += 1
+
+# Use up memory up to number MB, 1 MB at a time
+def memory_load(number):
+  i = 0
+  while i < number:
+    try:
+      a = ' ' * (i * MEGA)
+      del a
+    except MemoryError:
+      break
+    i += 1
+
+# Sleep for up to number millseconds
+def sleep_load(number):
+  sleep(randint(1,int(number))/1000) == 1
+
+
